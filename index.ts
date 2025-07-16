36992045562117e138ed5c3aac27ef63caf85838
Pedro Luis Frias Favero <Pedro.Luis.Frias.Favero+EYGS@es.ey.com>
2025-07-16 18:52:51 +0200

    chore: :wrench: Test prettier config

---
+++ index.ts
@@ -0,0 +1,10 @@
+const add = (a: number, b: number): number => a + b;
+
+console.log(add(1, 2));
+console.log(add('hello', 2));
+
+const flag = true;
+
+if (flag) {
+    console.log('Flag is true');
+}
