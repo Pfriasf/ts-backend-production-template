6a590652413092a909f4d67ae50ed61b02d17fd2
Pedro Luis Frias Favero <Pedro.Luis.Frias.Favero+EYGS@es.ey.com>
2025-08-08 09:36:31 +0200

    chore(eslint): :wrench: Change eslint config to inclu typr recommended check
    Improve eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,17 @@
 import js from '@eslint/js';
-import globals from 'globals';
 import tseslint from 'typescript-eslint';
+
+import globals from 'globals';
 import { defineConfig } from 'eslint/config';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default defineConfig([
-    { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], plugins: { js }, extends: ['js/recommended', eslintConfigPrettier] },
+    {
+        files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+        plugins: { js },
+        extends: ['js/recommended', eslintConfigPrettier],
+    },
     { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
     { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], languageOptions: { globals: globals.node } },
-    tseslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
 ]);
