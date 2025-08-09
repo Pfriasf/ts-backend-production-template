a49a4920399f54f05955e8e87835f96e4c50c695
Pedro Luis Frias Favero <Pedro.Luis.Frias.Favero+EYGS@es.ey.com>
2025-08-09 19:55:46 +0200

    feat(controller): :sparkles: Add apiController for main API logic
    Created apiController.ts to handle API endpoints and responses.

---
+++ src/controller/apiController.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from 'express';
+import httpResponse from '../util/httpResponse';
+import responseMessage from '../constant/responseMessage';
+import httpError from '../util/httpError';
+
+export default {
+    self: (req: Request, res: Response, next: NextFunction) => {
+        try {
+            httpResponse(req, res, 200, responseMessage.SUCCESS);
+        } catch (error) {
+            httpError(error, req, res, next, 500);
+        }
+    },
+};
