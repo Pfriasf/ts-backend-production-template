07c409852553e75ee342204d9fa2fbb1ef3f191b
Pedro Luis Frias Favero <Pedro.Luis.Frias.Favero+EYGS@es.ey.com>
2025-08-18 01:18:22 +0200

    feat(cors): :sparkles: Add CORS middleware for handling cross-origin requests

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,21 @@ import router from './router/apiRouter';
 import globalErrorHandler from './middleware/globalErrorHandler';
 import notFoundError from './util/notFoundError';
 import helmet from 'helmet';
+import cors from 'cors';
 
 const app: Application = express();
 
 // Security middleware
 app.use(helmet());
+
+// CORS middleware
+app.use(
+    cors({
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+        origin: ['http://localhost:3000'],
+        credentials: true,
+    }),
+);
 
 // Middleware to parse JSON bodies
 app.use(express.json());
